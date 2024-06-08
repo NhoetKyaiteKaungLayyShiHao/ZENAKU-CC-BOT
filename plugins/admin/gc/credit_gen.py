8e708859784f20d756234cbd1d6d9fb331b7917c
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-06-08 15:11:46 +0530

    Update credit_gen.py

--- plugins/admin/gc/credit_gen.py
+++ plugins/admin/gc/credit_gen.py
@@ -1,33 +1,92 @@
 from plugins.admin.gc.gc_func import *
 from pyrogram import Client, filters
+@Client.on_message(filters.command ('gc'))
+async def cmd_gc(client,message):
+  user_id = str(message.from_user.id)
+  CEO = "7427691214"
+  if user_id != CEO :
+    resp = "𝗥𝗲𝗾𝘂𝗶𝗿𝗲 𝗢𝘄𝗻𝗲𝗿 𝗣𝗿𝗶𝘃𝗶𝗹𝗮𝗴𝗲𝘀 ⚠️"
+    msg1 = await message.reply_text(resp,message.id)
+  else:
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴.."
+    send = await message.reply_text(resp,message.id)
+    GC1 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC1)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟭"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    GC2 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC2)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟮"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    GC3 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC3)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟯"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    GC4 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC4)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟰"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    GC5 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC5)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟱"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    GC6 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC6)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟲"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    GC7 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC7)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟳"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    GC8 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC8)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟴"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    GC9 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC9)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟵"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    GC10 = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
+    insert_pm(GC10)
+    resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 𝟭𝟬"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    resp = "𝗗𝗼𝗻𝗲"
+    send = await Client.edit_message_text(message.chat.id,send.id,resp)
+    final_resp = f"""
+𝗚𝗶𝗳𝘁𝗰𝗼𝗱𝗲 𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗲𝗱 ✅
+𝗔𝗺𝗼𝘂𝗻𝘁: 10
 
-@Client.on_message(filters.command('gc'))
-async def cmd_gc(client, message):
-    user_id = str(message.from_user.id)
-    CEO = "7427691214"
-    
-    if user_id != CEO:
-        resp = "𝗥𝗲𝗾𝘂𝗶𝗿𝗲 𝗢𝘄𝗻𝗲𝗿 𝗣𝗿𝗶𝘃𝗶𝗹𝗲𝗴𝗲𝘀 ⚠️"
-        await message.reply_text(resp, message.id)
-    else:
-        resp = "𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴.."
-        send = await message.reply_text(resp, message.id)
-        
-        gift_codes = []
-        
-        for i in range(1, 11):
-            resp = f"𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗶𝗻𝗴 {i}"
-            send = await client.edit_message_text(message.chat.id, send.id, resp)
-            
-            gc_code = f"XCC-{gcgenfunc()}-{gcgenfunc()}-{gcgenfunc()}"
-            insert_pm(gc_code)
-            gift_codes.append(gc_code)
-        
-        final_resp = "𝗚𝗶𝗳𝘁𝗰𝗼𝗱𝗲 𝗚𝗲𝗻𝗮𝗿𝗮𝘁𝗲𝗱 ✅\n𝗔𝗺𝗼𝘂𝗻𝘁: 10\n\n"
-        for i, gc_code in enumerate(gift_codes, 1):
-            final_resp += f"➔ <code>{gc_code}</code>\n𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺\n\n"
-        
-        final_resp += "𝗙𝗼𝗿 𝗥𝗲𝗱𝗲𝗲𝗺𝘁𝗶𝗼𝗻 \n𝗧𝘆𝗽𝗲 /redeem"
-        
-        await client.edit_message_text(message.chat.id, send.id, final_resp)
-      
+➔ <code>{GC1}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+➔ <code>{GC2}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+➔ <code>{GC3}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+➔ <code>{GC4}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+➔ <code>{GC5}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+➔ <code>{GC6}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+➔ <code>{GC7}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+➔ <code>{GC8}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+➔ <code>{GC9}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+➔ <code>{GC10}</code>
+𝗩𝗮𝗹𝘂𝗲 : 𝟭𝟬𝟬 𝗖𝗿𝗲𝗱𝗶𝘁𝘀 + 𝗣𝗿𝗲𝗺𝗶𝘂𝗺
+
+𝗙𝗼𝗿 𝗥𝗲𝗱𝗲𝗲𝗺𝘁𝗶𝗼𝗻 
+𝗧𝘆𝗽𝗲 /redeem
+    """
+    send = await Client.edit_message_text(message.chat.id,send.id,final_resp)
