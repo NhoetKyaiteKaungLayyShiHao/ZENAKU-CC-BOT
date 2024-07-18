5da9a183b3bc3f11f8c18bef5010ec862c2cd004
NhoetKyaiteKaungLayyShiHao <ohmyqueenmedusa15s@gmail.com>
2024-07-18 15:18:50 +0630

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ plugins = dict(root="plugins")
 
 async def main():
   user = Client("7427691214",
-                api_id="27649783",
-                api_hash="834fd6015b50b781e0f8a41876ca95c8")
-  bot = Client("7386696229:AAFS4tIvsbrBfTwmWIm1IVFcUz0OEUXeInw",
-               api_id="27649783",
-               api_hash="834fd6015b50b781e0f8a41876ca95c8",
-               bot_token="7386696229:AAFS4tIvsbrBfTwmWIm1IVFcUz0OEUXeInw",
+                api_id="1318826936",
+                api_hash="a0f8a2ed94ee61da2794717b586c8918")
+  bot = Client("7270877173:AAGYriWgvpilDO0IqYW6SEdzvKkmT_e60aM",
+               api_id="29852138",
+               api_hash="a0f8a2ed94ee61da2794717b586c8918",
+               bot_token="7270877173:AAGYriWgvpilDO0IqYW6SEdzvKkmT_e60aM",
                plugins=plugins)
   clients = [user, bot]
   bot.set_parse_mode(enums.ParseMode.HTML)
